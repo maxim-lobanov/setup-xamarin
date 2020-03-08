63f853e039d9d96ef99675adaad243a1654a3e08
Maxim Lobanov <v-malob@microsoft.com>
2020-03-08 20:48:18 +0300

    rework main file

--- src/setup-xamarin.ts
+++ src/setup-xamarin.ts
@@ -5,32 +5,25 @@ import { XamarinMacToolSelector } from './xamarin-mac-selector';
 import { XamarinAndroidToolSelector } from './xamarin-android-selector';
 import { ToolSelector } from './tool-selector';
 
-type SelectorResult = [ToolSelector, string];
-type SelectorClass = { new (): ToolSelector };
-
-const createToolSelector = (variableName: string, selectorClass: SelectorClass): SelectorResult | null => {
+const applySelector = (variableName: string, selectorClass: { new (): ToolSelector }): void => {
   const versionSpec = core.getInput(variableName, { required: false });
   if (!versionSpec) {
-    return null;
+    return;
   }
 
-  return [new selectorClass(), versionSpec];
+  const selector = new selectorClass();
+  const availableVersions = selector.getAllVersions();
+  const targetVersion = availableVersions[0];
+  selector.setVersion(targetVersion);
 }
 
 async function run() {
   try {
-    const selectors = [
-      createToolSelector('mono-version', MonoToolSelector),
-      createToolSelector('xamarin-ios-version', XamarinIosToolSelector),
-      createToolSelector('xamarin-mac-version', XamarinMacToolSelector),
-      createToolSelector('xamarin-android-version', XamarinAndroidToolSelector),
-    ].filter((sel): sel is SelectorResult => sel !== null);
+    applySelector('mono-version', MonoToolSelector);
+    applySelector('xamarin-ios-version', XamarinIosToolSelector);
+    applySelector('xamarin-mac-version', XamarinMacToolSelector);
+    applySelector('xamarin-android-version', XamarinAndroidToolSelector);
 
-    for (const [selector, version] of selectors) {
-      const availableVersions = selector.getAllVersions();
-      const targetVersion = availableVersions[0];
-      selector.setVersion(targetVersion);
-    }
   } catch (error) {
     core.setFailed(error.message);
   }
