a693b6ddad7227a48ce8014a74e80f5c6e30c750
Maxim Lobanov <v-malob@microsoft.com>
2020-03-08 16:25:21 +0300

    Update setup-xamarin.ts

--- src/setup-xamarin.ts
+++ src/setup-xamarin.ts
@@ -2,13 +2,15 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as core from '@actions/core';
 import {VersionRelativeDir, XamarinIOSDirectoryPath} from './constants';
+import * as semver from 'semver';
 
 async function run() {
   try {
     const xamarinVersionsDir = path.join(XamarinIOSDirectoryPath, VersionRelativeDir);
     const children: string[] = fs.readdirSync(xamarinVersionsDir);
     for (const child of children) {
-      console.log(child);
+        const valid = semver.valid(child);
+        console.log(`${child}: ${valid}`);
     }
   } catch (error) {
     core.setFailed(error.message);
