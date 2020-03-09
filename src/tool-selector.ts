08263e990611bb3a285b6b3067ec10c3e6fb6d2b
Maxim Lobanov <v-malob@microsoft.com>
2020-03-09 15:52:13 +0300

    qq

--- src/tool-selector.ts
+++ src/tool-selector.ts
@@ -38,7 +38,7 @@ export abstract class ToolSelector {
         core.debug(`Creating symlink '${currentVersionDirectory}' -> '${targetVersionDirectory}'`);
         if (fs.existsSync(currentVersionDirectory)) {
             //fs.unlinkSync(currentVersionDirectory);
-            const res = child.spawnSync('/usr/bin/sudo', [`rm -f ${currentVersionDirectory}`], );
+            const res = child.spawnSync('/usr/bin/sudo', ['rm', '-f', currentVersionDirectory], );
             console.log(res.status);
             console.log(res.stdout.toString());
             console.log(res.stderr.toString());
