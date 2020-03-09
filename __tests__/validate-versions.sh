e21ce641e69d408c383c960add43d31b9b504c52
Maxim Lobanov <v-malob@microsoft.com>
2020-03-09 17:56:38 +0300

    Update validate-versions.sh

--- __tests__/validate-versions.sh
+++ __tests__/validate-versions.sh
@@ -8,5 +8,5 @@ XAMARIN_ANDROID_VERSION=$4
 mono --version | grep "Mono JIT compiler version ${MONO_VERSION}"
 cat /Library/Frameworks/Mono.framework/Versions/Current/Version | grep $MONO_VERSION
 cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/Version | grep $XAMARIN_IOS_VERSION
-cat /Library/Frameworks/Xamarin.Mac.framework/Versions/Current/ersion | grep $XAMARIN_MAC_VERSION
+cat /Library/Frameworks/Xamarin.Mac.framework/Versions/Current/Version | grep $XAMARIN_MAC_VERSION
 cat /Library/Frameworks/Xamarin.Android.framework/Versions/Current/Version | grep $XAMARIN_ANDROID_VERSION
